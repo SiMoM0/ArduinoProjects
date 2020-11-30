fc544b32871f8e57ed9224ca8837e252ff0370c6
SiMoM0 <53409996+SiMoM0@users.noreply.github.com>
2020-11-30 13:37:02 +0100

    Add files via upload

---
+++ Project4/LedBreadAnalog.ino
@@ -0,0 +1,20 @@
+//Sketch per far accendere e spegnere gradualemnte il led collegato alla breadboard tramite il pin 11
+
+void setup() {
+  
+
+}
+
+//Utilizzo il pin 11 per analogWrite() 
+void loop() {
+  for(int i=0; i<255; i++) {
+    analogWrite(11, i);
+    delay(5);
+  }
+
+  for(int i=255; i>=0; i--) {
+    analogWrite(11, i);
+    delay(5);
+  }
+  delay(50);
+}
